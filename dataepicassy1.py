271876d821e34eb08b39b194e130ccda68041395
Korede <35337945+generalkorex@users.noreply.github.com>
2025-03-30 00:49:04 -0500

    Add files via upload

---
+++ dataepicassy1.py
@@ -0,0 +1,13 @@
+""" Task 1
+Write a simple Python program that checks whether each number from 1 to 15 is even or odd.
+"""
+
+for i in range(1,16):  
+
+
+    if i % 2 == 0:   # divisible by 2
+
+            print("The number is even", i)
+
+    else:
+        print("The number is Odd", i)
